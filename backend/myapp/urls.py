50c27221f0ff77c759c66eba125a4426da607a07
Minh2395 <vongominh2395@gmail.com>
2026-05-17 16:49:26 +0700

    update product-detail page and some css files

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from myapp.api import (home_api, product_api, news_api)
+from myapp.api import (home_api, news_api)
 from myapp.api.auth_api import (LoginAPI, RegisterAPI, LogoutAPI)
 from myapp.api.admin_api import (admin_dashboard_api, admin_users_api, admin_products_api, admin_orders_api, admin_stations_api)
+from myapp.api.product_api import product_detail_api
 
 urlpatterns = [
     # HOME
     path("home/", home_api.home_api),
 
     # PRODUCT
-    path("products/<int:product_id>/", product_api.product_detail_api),
+    path("products/<int:product_id>/", product_detail_api, name="product_detail_api"),
 
     # AUTH
     path("login/", LoginAPI.as_view(), name="api_login"),
